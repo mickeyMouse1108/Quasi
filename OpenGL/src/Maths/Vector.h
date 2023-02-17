c2817c2ee4c698ff7f48f472157ed9bec0b7a037
mickeyMouse1108 <chenyixian1108@gmail.com>
2023-02-17 21:10:18 +0800

    Removed float Vector to Generitize & Simplify Vector

--- OpenGL/src/Maths/Vector.h
+++ OpenGL/src/Maths/Vector.h
@@ -2,108 +2,178 @@
 
 namespace Maths
 {
-    struct Vector2;
-    struct Vector3;
-    struct Vector4;
-
     template <class T> struct Vec2;
     template <class T> struct Vec3;
     template <class T> struct Vec4;
+    using Vector2 = Vec2<float>;
+    using Vector3 = Vec3<float>;
+    using Vector4 = Vec4<float>;
+    using Vec2Int = Vec2<int>;
+    using Vec3Int = Vec3<int>;
+    using Vec4Int = Vec4<int>;
 
-#pragma region Vector2 Declaration
-    struct Vector2
+#pragma region Vec2 Declaration
+    template <class T> struct Vec2
     {
-        static const Vector2 RIGHT;
-        static const Vector2 LEFT ;
-        static const Vector2 UP   ;
-        static const Vector2 DOWN ;
+        static const Vec2 RIGHT;
+        static const Vec2 LEFT ;
+        static const Vec2 UP   ;
+        static const Vec2 DOWN ;
+        static const Vec2 ZERO ;
         
-        float x, y;
-        Vector2() : x(0), y(0) {}
-        Vector2(float x, float y = 0) : x(x), y(y) {}
-        Vector2(float val) : x(val), y(val) {}
-        Vector2(const Vector3& vec3);
+        T x, y;
+        Vec2() : x(0), y(0) {}
+        Vec2(T x, T y = 0) : x(x), y(y) {}
+        Vec2(T val) : x(val), y(val) {}
+        Vec2(const Vec3<T>& vec3);
 
-        template <class TOther> Vec2<TOther> Cast() { return {(TOther)x, (TOther)y}; }
+        template <class TOther> Vec2<TOther> Cast () { return {(TOther)x, (TOther)y}; }
+                                Vector2      CastF() { return {(float) x, (float) y}; }
 
-        float operator[] (unsigned int i) const { return ((const float*)this)[i]; }
-        operator float*() { return (float*)this; }
+        T operator[] (unsigned int i) const { return ((const T*)this)[i]; }
+        explicit operator T*() { return (T*)this; }
     };
-
-    Vector2 operator+ (const Vector2&a, const Vector2& b);
-    Vector2 operator- (const Vector2&a, const Vector2& b);
-    Vector2 operator* (const Vector2&a, const Vector2& b);
-    Vector2 operator/ (const Vector2&a, const Vector2& b);
-    Vector2 operator+ (const Vector2&a, float b);
-    Vector2 operator- (const Vector2&a, float b);
-    Vector2 operator* (const Vector2&a, float b);
-    Vector2 operator/ (const Vector2&a, float b);
+    
+    template <class T> Vec2<T> operator+ (const Vec2<T>& a, const Vec2<T>& b);
+    template <class T> Vec2<T> operator- (const Vec2<T>& a, const Vec2<T>& b);
+    template <class T> Vec2<T> operator* (const Vec2<T>& a, const Vec2<T>& b);
+    template <class T> Vec2<T> operator/ (const Vec2<T>& a, const Vec2<T>& b);
+    template <class T> Vec2<T> operator+ (const Vec2<T>& a, T b);
+    template <class T> Vec2<T> operator- (const Vec2<T>& a, T b);
+    template <class T> Vec2<T> operator* (const Vec2<T>& a, T b);
+    template <class T> Vec2<T> operator/ (const Vec2<T>& a, T b);
 #pragma endregion 
 
-#pragma region Vector3 Declaration
-    struct Vector3
+#pragma region Vec3 Declaration
+    template <class T> struct Vec3
     {
-        static const Vector3 RIGHT;
-        static const Vector3 LEFT ;
-        static const Vector3 UP   ;
-        static const Vector3 DOWN ;
-        static const Vector3 FRONT;
-        static const Vector3 BACK ;
+        static const Vec3 RIGHT;
+        static const Vec3 LEFT ;
+        static const Vec3 UP   ;
+        static const Vec3 DOWN ;
+        static const Vec3 FRONT;
+        static const Vec3 BACK ;
+        static const Vec3 ZERO ;
         
-        float x, y, z;
-        Vector3() : x(0), y(0), z(0) {}
-        Vector3(float x, float y, float z = 0) : x(x), y(y), z(z) {}
-        Vector3(float val) : x(val), y(val), z(val) {}
-        Vector3(const Vector2& vec2, float z) : x(vec2.x), y(vec2.y), z(z) {}
-        Vector3(const Vector4& vec4);
+        T x, y, z;
+        Vec3() : x(0), y(0), z(0) {}
+        Vec3(T x, T y, T z = 0) : x(x), y(y), z(z) {}
+        Vec3(T val) : x(val), y(val), z(val) {}
+        Vec3(const Vec2<T>& vec2, T z) : x(vec2.x), y(vec2.y), z(z) {}
+        Vec3(const Vec3<T>& vec3) : x(vec3.x), y(vec3.y), z(vec3.z) {}
+        Vec3(const Vec4<T>& vec4);
 
         template <class TOther> Vec3<TOther> Cast () { return {(TOther)x, (TOther)y, (TOther)z}; }
+                                Vector3      CastF() { return {(float) x, (float) y, (float) z}; }
 
-        float operator[] (unsigned int i) const { return ((const float*)this)[i]; }
-        operator float*() { return (float*)this; }
+        T operator[] (unsigned int i) const { return ((const T*)this)[i]; }
+        explicit operator T*() { return (T*)this; }
     };
 
-    Vector3 operator+ (const Vector3&a, const Vector3& b);
-    Vector3 operator- (const Vector3&a, const Vector3& b);
-    Vector3 operator* (const Vector3&a, const Vector3& b);
-    Vector3 operator/ (const Vector3&a, const Vector3& b);
-    Vector3 operator+ (const Vector3&a, float b);
-    Vector3 operator- (const Vector3&a, float b);
-    Vector3 operator* (const Vector3&a, float b);
-    Vector3 operator/ (const Vector3&a, float b);
-#pragma endregion 
+    template <class T> Vec3<T> operator+ (const Vec3<T>& a, const Vec3<T>& b);
+    template <class T> Vec3<T> operator- (const Vec3<T>& a, const Vec3<T>& b);
+    template <class T> Vec3<T> operator* (const Vec3<T>& a, const Vec3<T>& b);
+    template <class T> Vec3<T> operator/ (const Vec3<T>& a, const Vec3<T>& b);
+    template <class T> Vec3<T> operator+ (const Vec3<T>& a, T b);
+    template <class T> Vec3<T> operator- (const Vec3<T>& a, T b);
+    template <class T> Vec3<T> operator* (const Vec3<T>& a, T b);
+    template <class T> Vec3<T> operator/ (const Vec3<T>& a, T b);
+#pragma endregion
 
-#pragma region Vector4 Declaration
-    struct Vector4
+#pragma region Vec4 Declaration
+    template <class T> struct Vec4
     {
-        static const Vector4 RIGHT;
-        static const Vector4 LEFT ;
-        static const Vector4 UP   ;
-        static const Vector4 DOWN ;
-        static const Vector4 FRONT;
-        static const Vector4 BACK ;
-        static const Vector4 IN   ;
-        static const Vector4 OUT  ;
+        static const Vec4 RIGHT;
+        static const Vec4 LEFT ;
+        static const Vec4 UP   ;
+        static const Vec4 DOWN ;
+        static const Vec4 FRONT;
+        static const Vec4 BACK ;
+        static const Vec4 IN   ;
+        static const Vec4 OUT  ;
+        static const Vec4 ZERO ;
         
-        float x, y, z, w;
-        Vector4() : x(0), y(0), z(0), w(0) {}
-        Vector4(float x, float y, float z, float w = 0) : x(x), y(y), z(z), w(w) {}
-        Vector4(float val) : x(val), y(val), z(val), w(val) {}
-        Vector4(const Vector3& vec3, float w = 0) : x(vec3.x), y(vec3.y), z(vec3.z), w(w) {}
+        T x, y, z, w;
+        Vec4() : x(0), y(0), z(0), w(0) {}
+        Vec4(T x, T y, T z, T w = 0) : x(x), y(y), z(z), w(w) {}
+        Vec4(T val) : x(val), y(val), z(val), w(val) {}
+        Vec4(const Vec3<T>& vec3, T w = 0) : x(vec3.x), y(vec3.y), z(vec3.z), w(w) {}
 
-        template <class TOther> Vec2<TOther> Cast () { return {(TOther)x, (TOther)y, (TOther)z, (TOther)w}; }
+        template <class TOther> Vec4<TOther> Cast () { return {(TOther)x, (TOther)y, (TOther)z, (TOther)w}; }
+                                Vector4      CastF() { return {(float) x, (float) y, (float) z, (float) w}; }
 
-        float operator[] (unsigned int i) const { return ((const float*)this)[i]; }
-        operator float*() { return (float*)this; }
+        T operator[] (unsigned int i) const { return ((const T*)this)[i]; }
+        explicit operator T*() { return (T*)this; }
     };
 
-    Vector4 operator+ (const Vector4&a, const Vector4& b);
-    Vector4 operator- (const Vector4&a, const Vector4& b);
-    Vector4 operator* (const Vector4&a, const Vector4& b);
-    Vector4 operator/ (const Vector4&a, const Vector4& b);
-    Vector4 operator+ (const Vector4&a, float b);
-    Vector4 operator- (const Vector4&a, float b);
-    Vector4 operator* (const Vector4&a, float b);
-    Vector4 operator/ (const Vector4&a, float b);
-#pragma endregion 
+    template <class T> Vec4<T> operator+ (const Vec4<T>& a, const Vec4<T>& b);
+    template <class T> Vec4<T> operator- (const Vec4<T>& a, const Vec4<T>& b);
+    template <class T> Vec4<T> operator* (const Vec4<T>& a, const Vec4<T>& b);
+    template <class T> Vec4<T> operator/ (const Vec4<T>& a, const Vec4<T>& b);
+    template <class T> Vec4<T> operator+ (const Vec4<T>& a, T b);
+    template <class T> Vec4<T> operator- (const Vec4<T>& a, T b);
+    template <class T> Vec4<T> operator* (const Vec4<T>& a, T b);
+    template <class T> Vec4<T> operator/ (const Vec4<T>& a, T b);
+#pragma endregion
+
+#pragma region Vec2 Definition
+    template <class T> Vec2<T> const Vec2<T>::RIGHT = {  1,  0 };
+    template <class T> Vec2<T> const Vec2<T>::LEFT  = { -1,  0 };
+    template <class T> Vec2<T> const Vec2<T>::UP    = {  0,  1 };
+    template <class T> Vec2<T> const Vec2<T>::DOWN  = {  0, -1 };
+    template <class T> Vec2<T> const Vec2<T>::ZERO  = {  0,  0 };
+    
+    template <class T> Vec2<T>::Vec2(const Vec3<T>& vec3) : x(vec3.x), y(vec3.y) {}
+
+    template <class T> Vec2<T> operator+(const Vec2<T>& a, const Vec2<T>& b) { return {a.x + b.x, a.y + b.y}; }
+    template <class T> Vec2<T> operator-(const Vec2<T>& a, const Vec2<T>& b) { return {a.x - b.x, a.y - b.y}; }
+    template <class T> Vec2<T> operator*(const Vec2<T>& a, const Vec2<T>& b) { return {a.x * b.x, a.y * b.y}; }
+    template <class T> Vec2<T> operator/(const Vec2<T>& a, const Vec2<T>& b) { return {a.x / b.x, a.y / b.y}; }
+    template <class T> Vec2<T> operator+(const Vec2<T>& a, T b)              { return {a.x + b, a.y + b}; }
+    template <class T> Vec2<T> operator-(const Vec2<T>& a, T b)              { return {a.x - b, a.y - b}; }
+    template <class T> Vec2<T> operator*(const Vec2<T>& a, T b)              { return {a.x * b, a.y * b}; }
+    template <class T> Vec2<T> operator/(const Vec2<T>& a, T b)              { return {a.x / b, a.y / b}; }
+#pragma endregion
+
+#pragma region Vec3 Definition
+    template <class T> Vec3<T> const Vec3<T>::RIGHT = {  1,  0,  0 };
+    template <class T> Vec3<T> const Vec3<T>::LEFT  = { -1,  0,  0 };
+    template <class T> Vec3<T> const Vec3<T>::UP    = {  0,  1,  0 };
+    template <class T> Vec3<T> const Vec3<T>::DOWN  = {  0, -1,  0 };
+    template <class T> Vec3<T> const Vec3<T>::FRONT = {  0,  0,  1 };
+    template <class T> Vec3<T> const Vec3<T>::BACK  = {  0,  0, -1 };
+    template <class T> Vec3<T> const Vec3<T>::ZERO  = {  0,  0,  0 };
+    
+    template <class T> Vec3<T>::Vec3(const Vec4<T>& vec4) : x(vec4.x), y(vec4.y), z(vec4.z) {}
+
+    template <class T> Vec3<T> operator+(const Vec3<T>& a, const Vec3<T>& b) { return {a.x + b.x, a.y + b.y, a.z + b.z}; }
+    template <class T> Vec3<T> operator-(const Vec3<T>& a, const Vec3<T>& b) { return {a.x - b.x, a.y - b.y, a.z - b.z}; }
+    template <class T> Vec3<T> operator*(const Vec3<T>& a, const Vec3<T>& b) { return {a.x * b.x, a.y * b.y, a.z * b.z}; }
+    template <class T> Vec3<T> operator/(const Vec3<T>& a, const Vec3<T>& b) { return {a.x / b.x, a.y / b.y, a.z / b.z}; }
+    template <class T> Vec3<T> operator+(const Vec3<T>& a, T b)              { return {a.x + b, a.y + b, a.z + b}; }
+    template <class T> Vec3<T> operator-(const Vec3<T>& a, T b)              { return {a.x - b, a.y - b, a.z - b}; }
+    template <class T> Vec3<T> operator*(const Vec3<T>& a, T b)              { return {a.x * b, a.y * b, a.z * b}; }
+    template <class T> Vec3<T> operator/(const Vec3<T>& a, T b)              { return {a.x / b, a.y / b, a.z / b}; }
+#pragma endregion
+
+#pragma region Vec4 Definition
+    template <class T> Vec4<T> const Vec4<T>::RIGHT = {  1,  0,  0,  0 };
+    template <class T> Vec4<T> const Vec4<T>::LEFT  = { -1,  0,  0,  0 };
+    template <class T> Vec4<T> const Vec4<T>::UP    = {  0,  1,  0,  0 };
+    template <class T> Vec4<T> const Vec4<T>::DOWN  = {  0, -1,  0,  0 };
+    template <class T> Vec4<T> const Vec4<T>::FRONT = {  0,  0,  1,  0 };
+    template <class T> Vec4<T> const Vec4<T>::BACK  = {  0,  0, -1,  0 };
+    template <class T> Vec4<T> const Vec4<T>::IN    = {  0,  0,  0,  1 };
+    template <class T> Vec4<T> const Vec4<T>::OUT   = {  0,  0,  0, -1 };
+    template <class T> Vec4<T> const Vec4<T>::ZERO  = {  0,  0,  0,  0 };
+    
+    template <class T> Vec4<T> operator+(const Vec4<T>& a, const Vec4<T>& b) { return {a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w}; }
+    template <class T> Vec4<T> operator-(const Vec4<T>& a, const Vec4<T>& b) { return {a.x - b.x, a.y - b.y, a.z - b.z, a.w - b.w}; }
+    template <class T> Vec4<T> operator*(const Vec4<T>& a, const Vec4<T>& b) { return {a.x * b.x, a.y * b.y, a.z * b.z, a.w * b.w}; }
+    template <class T> Vec4<T> operator/(const Vec4<T>& a, const Vec4<T>& b) { return {a.x / b.x, a.y / b.y, a.z / b.z, a.w / b.w}; }
+    template <class T> Vec4<T> operator+(const Vec4<T>& a, T b)              { return {a.x + b, a.y + b, a.z + b, a.w + b}; }
+    template <class T> Vec4<T> operator-(const Vec4<T>& a, T b)              { return {a.x - b, a.y - b, a.z - b, a.w - b}; }
+    template <class T> Vec4<T> operator*(const Vec4<T>& a, T b)              { return {a.x * b, a.y * b, a.z * b, a.w * b}; }
+    template <class T> Vec4<T> operator/(const Vec4<T>& a, T b)              { return {a.x / b, a.y / b, a.z / b, a.w / b}; }
+#pragma endregion
 }

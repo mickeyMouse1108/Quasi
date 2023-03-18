d0ec2ce01f0d0225eb88955ed7fb6bb806c4896d
mickeyMouse1108 <chenyixian1108@gmail.com>
2023-03-18 18:47:32 +0800

    Created More JSON Serializing Utilities for Array and Optional & Block Texture Dispatching

--- OpenGL/src/Game/Serialization/BlockTextureSerialization.h
+++ OpenGL/src/Game/Serialization/BlockTextureSerialization.h
@@ -1,7 +1,10 @@
 ﻿#pragma once
 #include "Vector.h"
 #include "VectorSerialization.h"
+#include "SerializationUtils.h"
 #include "nlohmann/json.hpp"
+
+#pragma region TextureAtlasID
 
 namespace Game::Serialization {
     struct TextureAtlasID {
@@ -28,10 +31,15 @@ struct nlohmann::adl_serializer<Game::Serialization::TextureAtlasID> {
     }
 };
 
+#pragma endregion
+
+#pragma region BlockTextureStructure
+
 namespace Game::Serialization {
     struct BlockTextureStructure {
+        template <class T> using option = std::optional<T>;
         TextureAtlasID all;
-        std::array<TextureAtlasID, 6> sides = {-1, -1, -1, -1, -1, -1};
+        std::array<option<TextureAtlasID>, 6> sides;
     };
 }
 
@@ -41,10 +49,7 @@ struct nlohmann::adl_serializer<Game::Serialization::BlockTextureStructure> {
     using BlockTexture = Game::Serialization::BlockTextureStructure;
     static void from_json(const json& js, BlockTexture& bs) {
         js.at("all").get_to(bs.all);
-        const auto sides = js.at("sides");
-        for (int i = 0; i < 6; ++i) {
-            sides.at(i).get_to(bs.sides[i]);
-        }
+        js.at("sides").get_to(bs.sides);
     }
 
     static void to_json(json& js, const BlockTexture& bs) {
@@ -54,3 +59,106 @@ struct nlohmann::adl_serializer<Game::Serialization::BlockTextureStructure> {
         };
     }
 };
+
+#pragma endregion
+
+#pragma region BlockStateMatchingStructure
+
+// ! to prevent include looping: BlockBase.h -> BlockRenderer.h -> BlockTextureSerialization.h
+namespace Game {
+    enum class BlockType;
+}
+
+namespace Game::Serialization {
+    struct BlockStateMatchingStructure {
+        template <class T> using option = std::optional<T>;
+        option<Maths::Vec3Int> position;
+        std::array<std::optional<BlockType>, 6> facing = { };
+    };
+}
+
+template <>
+struct nlohmann::adl_serializer<Game::Serialization::BlockStateMatchingStructure> {
+    using BlockMatch = Game::Serialization::BlockStateMatchingStructure;
+    static void from_json(const json& js, BlockMatch& bm) {
+        js.at("position").get_to(bm.position);
+        js.at("facing").get_to(bm.facing);
+    }
+
+    static void to_json(json& js, const BlockMatch& bm) {
+        js = {
+            { "position", bm.position },
+            { "sides", bm.facing }
+        };
+    }
+};
+
+#pragma endregion
+
+#pragma region BlockTextureDispatcher
+
+namespace Game::Serialization {
+    struct BlockTextureDispatcher {
+        BlockType type;
+        BlockTextureStructure defaultTexture;
+        std::vector<BlockStateMatchingStructure> stateDispatch;
+    };
+}
+
+template struct nlohmann::adl_serializer<Game::BlockType>;
+
+template <>
+struct nlohmann::adl_serializer<Game::Serialization::BlockTextureDispatcher> {
+    using BlockDisp = Game::Serialization::BlockTextureDispatcher;
+    static void from_json(const json& js, BlockDisp& bp) {
+        js.at("type").get_to(bp.type);
+        js.at("default").get_to(bp.defaultTexture);
+        js.at("when").get_to(bp.stateDispatch);
+    }
+
+    static void to_json(json& js, const BlockDisp& bp) {
+        js = json {
+            { "type", (int)bp.type },
+            { "default",   bp.defaultTexture },
+            { "when",      bp.stateDispatch }
+        };
+    }
+};
+
+#pragma endregion
+
+#pragma region TextureDispatcher
+
+namespace Game::Serialization {
+    struct TextureDispatcher {
+        Maths::Vec2Int textureAtlasSize = {};
+        std::unordered_map<BlockType, BlockTextureDispatcher> blockTextures;
+    };
+}
+
+template <>
+struct nlohmann::adl_serializer<Game::Serialization::TextureDispatcher> {
+    using TextureDisp = Game::Serialization::TextureDispatcher;
+    using BlockDisp = Game::Serialization::BlockTextureDispatcher;
+    static void from_json(const json& js, TextureDisp& tp) {
+        js.at("texSize").get_to(tp.textureAtlasSize);
+        for (const auto& btex : js.at("textures")) {
+            BlockDisp bp = {};
+            btex.get_to<BlockDisp>(bp);
+            tp.blockTextures.emplace(bp.type, bp);
+        }
+    }
+
+    static void to_json(json& js, const TextureDisp& tp) {
+        js = json {
+            { "texSize",  tp.textureAtlasSize },
+            { "textures", {} }
+        };
+        // funky code
+        for (const auto& [ _, bp ] : tp.blockTextures) {
+            js.at("textures") += { bp };
+        }
+    }
+};
+
+#pragma endregion

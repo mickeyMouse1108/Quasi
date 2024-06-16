61cef3f0325d760c71a4c79ac359796ddba558f4
mickeyMouse1108 <chenyixian1108@gmail.com>
2024-06-16 21:46:13 +0800

    removed unnecessary scope in main()

--- Testing/Testing.cpp
+++ Testing/Testing.cpp
@@ -3,12 +3,10 @@
 int main(int argc, char* argv[]) {
     Debug::Info("Execution Directory: {}", argv[0]);
     Debug::Info("Project Directory: {}", Test::PROJECT_DIRECTORY);
-    {
-        Test::TestManager tManager;
 
-        tManager.OnInit();
-        while (tManager.WindowIsOpen())
-            tManager.OnRun();
-        tManager.OnDestroy();
-    }
+    Test::TestManager tManager;
+    tManager.OnInit();
+    while (tManager.WindowIsOpen())
+        tManager.OnRun();
+    tManager.OnDestroy();
 }
